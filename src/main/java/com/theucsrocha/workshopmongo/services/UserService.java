4655debe423c16bae871bb3781170b168e37f88c
Matheus Rocha <theucsrocha@gmail.com>
2025-09-23 14:39:36 -0300

    Obtendo usuario por id

--- src/main/java/com/theucsrocha/workshopmongo/services/UserService.java
+++ src/main/java/com/theucsrocha/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.theucsrocha.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.theucsrocha.workshopmongo.domain.User;
 import com.theucsrocha.workshopmongo.repository.UserRepository;
+import com.theucsrocha.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,15 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 
+	}
+	
+	public User findById(String id) {
+	    Optional<User> userOptional = repo.findById(id);
+
+	    if (userOptional.isPresent()) {
+	        return userOptional.get();
+	    } else {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
 	}
 }
