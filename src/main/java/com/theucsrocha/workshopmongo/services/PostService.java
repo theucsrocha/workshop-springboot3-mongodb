fcdf292085d7b5d8eccfaade70b1e95e475f3eae
Matheus Rocha <theucsrocha@gmail.com>
2025-09-30 14:56:14 -0300

    Consulta simples com query methods

--- src/main/java/com/theucsrocha/workshopmongo/services/PostService.java
+++ src/main/java/com/theucsrocha/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.theucsrocha.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,10 @@ public class PostService {
 	    } else {
 	        throw new ObjectNotFoundException("Objeto não encontrado");
 	    }
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContaining(text);
 	}
 	
 
